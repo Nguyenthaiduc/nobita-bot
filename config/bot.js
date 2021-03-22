58fd8ee27ba95897efcb91098e8f9d488574d598
Nguyenthaiduc <73944631+Nguyenthaiduc@users.noreply.github.com>
2021-03-22 16:20:57 +0700

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'ODIzMDcxMDAyMTQ5NjUwNDUz.YFbemg.qgtecq3yq9M85Hju_w5U2GnVMx0',
+        prefix: '??',
+        activity: 'Nguyễn Thái Đức',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
